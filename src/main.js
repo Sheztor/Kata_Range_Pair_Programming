e8cbcbfbf5cc35b79d9bafcb8b8c27da1a49c9e9
Sheztor <hfsa2327@gmail.com>
2024-02-28 16:08:15 -0400

    Adding success scenarios - Finishing the program

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 function DeadAnts(ants){
    if(!ants || typeof ants !=="string") return -1;
+
+   let countA = 0, countN = 0, countT = 0;
+
+   for(i = 0; i < ants.length; i++){
+    const currentChar = ants[i]
+
+    if(currentChar === "a" && ants[i+1] === "n" && ants[i+2] === "t"){
+      i += 2
+      continue
+    } // ant 
+
+    if(currentChar === "a") countA++
+    else if (currentChar === "n") countN++
+    else if (currentChar === "t") countT++
+   }
+
+   return Math.max(countA, countN, countT)
 }
 
 module.exports = DeadAnts;
--- src/main.test.js
+++ src/main.test.js
@@ -16,3 +16,20 @@ test("Return -1 if the string is empty", () =>{
 test("Return -1 if the input is not a string", () =>{
     expect(DeadAnts(12)).toBe(-1);
 })
+
+test("Return 1 if the input has a dead ant", () =>{
+  expect(DeadAnts("ant ant nt")).toBe(1);
+})
+
+
+test("Return 2 if the input has 2 dead ants", () =>{
+  expect(DeadAnts("an ant ant tn")).toBe(2);
+})
+
+test("Return 0 if the input has not dead ants", () =>{
+  expect(DeadAnts("ant ant ant")).toBe(0);
+})
+
+test("Return 0 if the input is 'Helo world' dead ants", () =>{
+  expect(DeadAnts("'Helo world'")).toBe(0);
+})
