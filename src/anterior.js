b0a3ebb5d8927c6df67dd3a85e880fba9cfbd1e5
Sheztor <hfsa2327@gmail.com>
2024-02-28 13:19:25 -0400

    Plan de prueba y codigo anterior

---
+++ src/anterior.js
@@ -0,0 +1,17 @@
+function CountDeadAnts(ants) {
+  if(ants !== null) ants = ants.toString().replace(/ant/g, "") // Delete all ants
+
+  if(ants === null || ants.trim() === "") return 0
+
+  const heads = ants.split(/a/g).length - 1
+  const bodies = ants.split(/n/g).length - 1
+  const butt = ants.split(/t/g).length - 1
+
+  let deadAnts = heads
+  if(heads < bodies) deadAnts = bodies
+  if(bodies < butt) deadAnts = butt
+
+  return deadAnts
+}
+
+console.log(CountDeadAnts("ant ant ant a"))
