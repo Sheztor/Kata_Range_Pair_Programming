83358b2bb9fe827930f60ab2878fc42881c940e3
Sheztor <hfsa2327@gmail.com>
2024-03-05 16:56:30 -0400

    An added test case and small backtracking in the main code

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 function DeadAnts(ants){
    if(!ants || typeof ants !=="string") return -1;
 
-   let countA = 0;
-   let countN = 0;
-   let countT = 0;
+   let countA = 0, countN = 0, countT = 0
+   const lengthAnst = ants.length
 
-   for (let i = 0; i < ants.length; i++) {
-      if (ants.substring(i, 3) === 'ant') {
-          i += 2; // Saltar las letras 'a', 'n', 't'
-      } else {
-          if (ants[i] === 'a') countA++;
-          if (ants[i] === 'n') countN++;
-          if (ants[i] === 't') countT++;
-      }
+   for (let i = 0; i < lengthAnst; i++) {
+    const currentChar = ants[i]
+    
+    if (ants[i] === "a" && ants[i+1] === "n" && ants[i+2] === "t") {
+      i += 2
+      continue
+    } // ant 
+
+    if(currentChar === "a") countA++
+    else if (currentChar === "n") countN++
+    else if (currentChar === "t") countT++
+
   }
 
    return Math.max(countA, countN, countT)
-
-   // let deadAnts = countA
-   // if(countA < countN) deadAnts = countN
-   // if(countN < countT) deadAnts = countT
-   // return deadAnts;
 
 }
 
--- src/main.test.js
+++ src/main.test.js
@@ -3,40 +3,44 @@ const DeadAntsOld = require('./anterior');
 const measureExecutionTime = require('./performance');
 
 
-// test("Return -1 if the string is null", () =>{
-//     expect(DeadAnts(null)).toBe(-1);
-// })
+test("Return -1 if the string is null", () =>{
+    expect(DeadAnts(null)).toBe(-1);
+})
 
-// test("Return -1 if the string is undefined", () =>{
-//     expect(DeadAnts(undefined)).toBe(-1);
-// })
+test("Return -1 if the string is undefined", () =>{
+    expect(DeadAnts(undefined)).toBe(-1);
+})
 
-// test("Return -1 if the string is empty", () =>{
-//     expect(DeadAnts("")).toBe(-1);
-// })
+test("Return -1 if the string is empty", () =>{
+    expect(DeadAnts("")).toBe(-1);
+})
 
-// test("Return -1 if the input is not a string", () =>{
-//     expect(DeadAnts(12)).toBe(-1);
-// })
-
-
-// test("Return 2 if the input has 2 dead ants", () =>{
-//   expect(DeadAnts("an ant ant tn")).toBe(2);
-// })
-
-// test("Return 0 if the input has not dead ants", () =>{
-//   expect(DeadAnts("ant ant ant")).toBe(0);
-// })
-
-// test("Return 0 if the input is 'Helo world' dead ants", () =>{
-//   expect(DeadAnts("'Helo world'")).toBe(0);
-// })
+test("Return -1 if the input is not a string", () =>{
+    expect(DeadAnts(12)).toBe(-1);
+})
 
 
-test("Compare execution speed of DeadAnts and DeadAntsOld", () => {
-  const input = "'antantatnatattatatatatantantat".repeat(10000);
-  const timeOld = measureExecutionTime(DeadAntsOld, input);
-  const timeNew = measureExecutionTime(DeadAnts, input);
+test("Return 2 if the input has 2 dead ants", () =>{
+  expect(DeadAnts("an ant ant tn")).toBe(2);
+})
+
+test("Return 0 if the input has not dead ants", () =>{
+  expect(DeadAnts("ant ant ant")).toBe(0);
+})
+
+test("Return 0 if the input is 'Helo world' dead ants", () =>{
+  expect(DeadAnts("'Helo world'")).toBe(0);
+})
+
+test("Returns 1 if the entry contains an 'an' at the end", () =>{
+  expect(DeadAnts("ant ant ant an")).toBe(1);
+})
+
+
+// test("Compare execution speed of DeadAnts and DeadAntsOld", () => {
+//   const input = "'antantatnatattatatatatantantat".repeat(10000);
+//   const timeOld = measureExecutionTime(DeadAntsOld, input);
+//   const timeNew = measureExecutionTime(DeadAnts, input);
  
-  expect(timeNew).toBeLessThan(timeOld);
-});
+//   expect(timeNew).toBeLessThan(timeOld);
+// });
