5d4fac79e280f16445a932708876f77d63b12194
Sheztor <hfsa2327@gmail.com>
2024-03-07 23:05:48 -0400

    Mas informacion mostrada del benchmark (Mean, Error, StdDev, etc.)

--- src/benchmark.js
+++ src/benchmark.js
@@ -19,6 +19,16 @@ suite.add("Dead Ants New", () => {
 
 suite.on("complete", function() {
   console.info("Fastest is: " + this.filter('fastest').map('name'))
+
+  let output = "\n"
+  output += "| Method         |       Runtime |       N |       Mean |     Error |   StdDev |\n"
+  output += "|--------------- |-------------- |-------- |-----------:|----------:|---------:|\n"
+  this.forEach(element => {
+    element.count = 100
+    output += `| ${element.name}  | ${element.hz.toFixed(2)} ops/s  | ${element.count.toFixed(2)}  |  ${(element.stats.mean* 1000).toFixed(4) } s  | ${(element.stats.moe * 1000).toFixed(4) } s  | ${(element.stats.deviation * 1000).toFixed(4)} s |\n`
+  })
+
+  console.log(output)
 })
 
 
